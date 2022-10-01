0967503cb3558f4453da5594512a6229467f16e1
ps8847 <ps789650@gmail.com>
2022-10-01 21:31:49 +0530

    created Yt Content Downloader

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-var link = document.querySelector('.link');
-var format = document.querySelector('.format');
+var link = document.querySelector(".link");
+var format = document.querySelector(".format");
 
-function download(){
-	if(link.value!=""){
-		var url1 = link.value.replace("https://youtube/","https://www.youtube.com/embed/");
+function download() {
+  if (link.value != "") {
+    var url = link.value;
 
-		console.log(url1);
+    var casee;
+    var id;
+    if (url.includes("v=")) {
+      casee = url.split("v=")[1];
+      id = casee;
+    }
+    if (url.includes(".be/")) {
+      casee = url.split(".be/")[1];
+      id = casee;
+    }
+    if (url.includes("shorts/")) {
+      casee = url.split("shorts/")[1].substring(0, 11);
+      id = casee;
+    }
 
-		document.querySelector('.result1').innerHTML = `<iframe width="100%" height="200" src="${url1}" title="Youtube Video Player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    document.querySelector(
+      ".result1"
+    ).innerHTML = `<img id="vidImg" src="https://img.youtube.com/vi/${id}/hqdefault.jpg" height="450px" width="97%" style=" margin:10px; border-radius:10px; align-items:center; justify-content:center;">`;
 
-		var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
+    var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
 
-		document.querySelector('.result2').innerHTML = `<iframe style="width:100%; height:50px; border:hidden; overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
-	}
+    document.querySelector(
+      ".result2"
+    ).innerHTML = `<iframe style="width:100%; height:50px; border:hidden; overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
+  }
 }
-
